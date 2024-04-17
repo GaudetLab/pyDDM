a0a6df1dafe88c466aae24967a99cb2f5e7a2d43
Sam Berry <61884003+samberry19@users.noreply.github.com>
2024-04-17 11:34:05 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='pyDDM',
+      version='0.2.0',
+      author='Sam Berry',
+      author_email="sberry@g.harvard.edu",
+      description = "Calculate distance difference matrices to compare protein conformational changes",
+      py_modules=["pyddm"]
+      )
