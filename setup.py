c47099d99ea69f2af066eb037c2f608b17af3f6d
Sam Berry <61884003+samberry19@users.noreply.github.com>
2025-09-01 15:45:24 -0400

    Refactor setup.py to use setuptools
    Updated setup.py to use setuptools and added metadata.

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
-from distutils.core import setup
-setup(name='pyddm',
-      version='1.1',
-      author='Sam Berry',
-      author_email="sberry@g.harvard.edu",
-      description = "",
-      py_modules=["pyDDM"]
+# setup.py
+from pathlib import Path
+from setuptools import setup
+
+setup(
+    name="pyddm",
+    version="1.1.0",
+    author="Sam Berry",
+    author_email="sberry@g.harvard.edu",
+    description="",
+    py_modules=["pyDDM.ddm"],
+    python_requires=">=3.8",
+    url="https://github.com/GaudetLab/pyDDM",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
